60a8c9086b40a4933e5618c2a8379b18cf10a0a5
MohabGhobashy <mohab.ali01@eng-st.cu.edu.eg>
2022-12-26 22:34:13 +0200

    upload photos

--- code/app.py
+++ code/app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template, send_file, request, redirect
 import os
+from flask import Flask, request, render_template
+
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', "POST", "PUT"])
-def index():
+
+@app.route('/')
+def home():
     return render_template('index.html')
 
+
+@app.route('/image', methods=['GET', 'POST'])
+def upload_file():
+
+    file1 = request.files['file']
+    Image = file1.save(os.path.join('assets/image.png'))
+
+    return []
+
+
+@app.route('/image2', methods=['GET', 'POST'])
+def upload_file2():
+
+    file2 = request.files['file']
+    Image = file2.save(os.path.join('assets/image2.png'))
+
+    return []
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
